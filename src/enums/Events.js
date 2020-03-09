e59d87a28065b2d4a446c5f2f487d96602dd3213
Vinicius Gomes <vinigomescunha@gmail.com>
2020-03-09 12:52:26 -0300

    Adicao evento desafio p2

--- src/enums/Events.js
+++ src/enums/Events.js
@@ -1,6 +1,7 @@
 // Como e uma pagina somente nao tem rota
 const EVENTS = {
   DISPLAY_CHAT: 'mostrar o chat',
-  DISPLAY_CONTACT: 'display pagina de contato'
+  DISPLAY_CONTACT: 'display pagina de contato',
+  DISPLAY_DESAFIO_P2: 'mostrar desafio parte 2'
 };
 export default EVENTS;
